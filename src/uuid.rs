3e1d2dbca5d7a9f6ebca1a9f66e42f21c152b8cf
WanShiTong <b.wan.shi.tong@proton.me>
2024-05-07 20:53:59 +0200

    Updated uuid

--- src/uuid.rs
+++ src/uuid.rs
@@ -4,8 +4,12 @@ const BASE_UUID: (u32, u16, u16, &[u8; 8]) = (0, 0, 0x1000, b"\x80\x00\x00\x80\x
 
 pub trait SdpShortUuid<T: Into<u32>> {
     fn from_sdp_short_uuid(uuid: T) -> Uuid {
-        Uuid::from_fields(uuid.into(), BASE_UUID.1, BASE_UUID.2, BASE_UUID.3).unwrap()
+        uuid_from_sdp(uuid.into())
     }
+}
+
+pub const fn uuid_from_sdp(uuid: u32) -> Uuid{
+    Uuid::from_fields(uuid, BASE_UUID.1, BASE_UUID.2, BASE_UUID.3)
 }
 
 impl SdpShortUuid<u16> for Uuid {}
